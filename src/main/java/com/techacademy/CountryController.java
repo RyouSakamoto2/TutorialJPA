ea79c9a53624f6d91019134a8e233612fc074beb
smoto <smoto@Ryo-Sakamoto-PC>
2023-04-01 23:48:28 +0900

    "Spring Data JPAの利用"課題提出用コミット

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -25,41 +25,40 @@ public class CountryController {
         // country/list.htmlに画面遷移
         return "country/list";
     }
- // ----- 詳細画面 -----
+
+    // ----- 詳細画面 -----
     @GetMapping(value = { "/detail", "/detail/{code}/" })
-    public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+    public String getCountry1(@PathVariable(name = "code", required = false) String code, Model model) {
         // codeが指定されていたら検索結果、無ければ空のクラスを設定
-        Country country = code != null ? service.getCountry(code) : new Country();
+        Country country1 = code != null ? service.getCountry(code) : new Country();
         // Modelに登録
-        model.addAttribute("country", country);
+        model.addAttribute("country", country1);
         // country/detail.htmlに画面遷移
         return "country/detail";
     }
-
     // ----- 更新（追加） -----
     @PostMapping("/detail")
-    public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name,
-            @RequestParam("population") int population, Model model) {
+    public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name, @RequestParam("population") int population, Model model) {
         // 更新（追加）
         service.updateCountry(code, name, population);
-
         // 一覧画面にリダイレクト
         return "redirect:/country/list";
     }
-
     // ----- 削除画面 -----
-    @GetMapping("/delete")
-    public String deleteCountryForm(Model model) {
+    @GetMapping(value = { "/delete", "/delete/{code}/" })
+    public String getCountry2(@PathVariable(name = "code", required = false) String code, Model model) {
+        // codeが指定されていたら検索結果を設定
+        Country country2 = code != null ? service.getCountry(code) : new Country();
+        // Modelに登録
+        model.addAttribute("country", country2);
         // country/delete.htmlに画面遷移
         return "country/delete";
     }
-
     // ----- 削除 -----
     @PostMapping("/delete")
-    public String deleteCountry(@RequestParam("code") String code, Model model) {
+    public String deleteCountry(@RequestParam("code") String code){
         // 削除
         service.deleteCountry(code);
-
         // 一覧画面にリダイレクト
         return "redirect:/country/list";
     }
